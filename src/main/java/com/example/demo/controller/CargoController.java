2567634804695fd393315768f6f204228ffb0c93
Leo Chu <leochu2010@gmail.com>
2024-09-15 19:55:24 +0800

    allow cross origin from reactjs

--- src/main/java/com/example/demo/controller/CargoController.java
+++ src/main/java/com/example/demo/controller/CargoController.java
@@ -3,11 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.kafka.Cargo;
 import com.example.demo.kafka.CargoConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class CargoController {
 
